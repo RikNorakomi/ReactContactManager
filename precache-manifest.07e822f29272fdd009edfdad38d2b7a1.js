635cd568fd11420940c042b8dfeeb6ea83580a68
rikvanvelzen <rikvanvelzen@yahoo.com>
2020-01-15 19:06:58 +0100

    Updates

---
+++ precache-manifest.07e822f29272fdd009edfdad38d2b7a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4d968ddaeb3271bf936fc326d3a49f",
+    "url": "/ReactContactManager/index.html"
+  },
+  {
+    "revision": "36ef1efac93050487a05",
+    "url": "/ReactContactManager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4d7fae6882f82f6f1604",
+    "url": "/ReactContactManager/static/css/main.ee6f9e91.chunk.css"
+  },
+  {
+    "revision": "36ef1efac93050487a05",
+    "url": "/ReactContactManager/static/js/2.0f5131bd.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/ReactContactManager/static/js/2.0f5131bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d7fae6882f82f6f1604",
+    "url": "/ReactContactManager/static/js/main.ff8f86e8.chunk.js"
+  },
+  {
+    "revision": "9c8ff46e5b86b2744cf7",
+    "url": "/ReactContactManager/static/js/runtime-main.0853a6e7.js"
+  }
+]);
